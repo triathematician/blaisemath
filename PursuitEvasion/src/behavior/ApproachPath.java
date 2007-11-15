87bc33fb8b1d5414c2fc0a68b09e27063b8e0ec7
ae3263 <ae3263@031c92da-8645-0410-8243-e70c107b35c0>
2007-11-15 18:16:26 +0000

    Updated to work entirely with blaise (Blaise-Old is no longer necessary). Implemented a value graph to display the team's "value" function. Working on integrating multiple goals, event logs, and task assignment/consolidation.

---
+++ PursuitEvasion/src/behavior/ApproachPath.java
@@ -0,0 +1,21 @@
+/*
+ * FixedPath.java
+ * Created on Aug 28, 2007, 11:21:31 AM
+ */
+
+package behavior;
+
+import behavior.*;
+import simulation.Agent;
+import scio.coordinate.R2;
+import scio.coordinate.V2;
+
+/**
+ * @author Elisha Peterson<br><br>
+ * 
+ * Moves on a fixed path relative to time rather than position.
+ * Follows the path as best as possible. If unable to follow the path, heads towards where it SHOULD be at that time.
+ */
+public class ApproachPath extends Behavior {  
+    public R2 direction(Agent self,V2 target,double t){return self.getPositionTime(t).minus(self.loc).normalized();}
+}
