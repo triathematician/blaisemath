0143e0d08f26686e39740805722b7606030eb9b5
lucas.gebhart@usma.edu <lucas.gebhart@usma.edu@031c92da-8645-0410-8243-e70c107b35c0>
2008-06-06 20:08:33 +0000

    Resolved Conflicts

--- PursuitEvasion/src/tasking/ControlOptimal.java
+++ PursuitEvasion/src/tasking/ControlOptimal.java
@@ -2,13 +2,22 @@
  * Closest.java
  * Created on Sep 4, 2007, 2:46:01 PM
  */
-
 package tasking;
 
+import drasys.or.mp.Constraint;
+import drasys.or.mp.ConstraintI;
+import drasys.or.mp.DuplicateException;
+import drasys.or.mp.NotFoundException;
+import drasys.or.mp.Problem;
+import drasys.or.mp.SizableProblemI;
+import drasys.or.mp.Variable;
+import drasys.or.mp.VariableI;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import simulation.Agent;
 import java.util.Vector;
 import simulation.Team;
-import utility.AgentPair;
 import utility.DistanceTable;
 
 /**
@@ -18,9 +27,11 @@ import utility.DistanceTable;
  */
 public class ControlOptimal extends TaskGenerator {
 
-    public ControlOptimal(Team target,int type){ super(target,type); }
-    
-   /** Assigns tasks (prey) to n/k pursuers. The algorithm uses the complete table
+    public ControlOptimal(Team target, int type) {
+        super(target, type);
+    }
+
+    /** Assigns tasks (prey) to n/k pursuers. The algorithm uses the complete table
      * of distances between pursuers and evaders to map the closest
      * pursuer-prey pair. This process is repeated until there are no more
      * pursuers or prey available. If there are leftover pursuers, they are
@@ -28,24 +39,80 @@ public class ControlOptimal extends TaskGenerator {
      * @param team pursuing team
      */
     @Override
-    public void generate(Vector<Agent> team, DistanceTable table, double priority) {
-        DistanceTable dist=new DistanceTable(team,target);  
-//        dist.
-//        // assign prey by closest-to-pursuer first
-//        Vector<Agent> ps=new Vector<Agent>(team);
-//        Vector<Agent> es=new Vector<Agent>(target.getActiveAgents());
-//        AgentPair closest;
-//        do{        
-//            closest=dist.min(ps,es);
-//            if(closest!=null){
-//                closest.getFirst().assign(new Task(this,closest.getSecondLoc(),goalType,priority));
-//                ps.remove(closest.getFirst());
-//                es.remove(closest.getSecond());
-//            }
-//        }while(!ps.isEmpty()&&!es.isEmpty()&&closest!=null);
-//        // assign remaining pursuers to closest prey
-//        for(Agent p:ps){
-//            p.assign(new Task(this,dist.min(p,target).getSecondLoc(),goalType,priority));
-//        }
+    public void generate(Collection<Agent> team, DistanceTable table, double priority) {
+        Vector<Agent> ps = new Vector<Agent>(team);
+        Vector<Agent> es = new Vector<Agent>(target.getActiveAgents());
+        DistanceTable dist = new DistanceTable(ps, es);
+        int numberOfPursuers = ps.size();
+        int numberOfEvaders = es.size();
+
+        //  This defines (how many constaints, how many variables)      
+        SizableProblemI prob = new Problem(numberOfPursuers + numberOfEvaders,
+                numberOfPursuers * numberOfEvaders);
+        prob.getMetadata().put("lp.isMinimize", "true");
+
+        for (int i = 0; i < numberOfPursuers; i++) {
+            for (int j = 0; j < numberOfEvaders; j++) {
+                try {
+                    prob.newVariable("x" + i + " " + j).setObjectiveCoefficient(dist.get(i, j));
+                } catch (DuplicateException ex) {
+                    Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+
+        final byte LTE = Constraint.LESS;
+        final byte GTE = Constraint.GREATER;
+        final byte EQL = Constraint.EQUAL;
+
+        if (numberOfPursuers >= numberOfEvaders) {
+            for (int j = 0; j < numberOfEvaders; j++) {
+                try {
+                    prob.newConstraint(j + "is chased").setType(GTE).setRightHandSide(1.0);
+                } catch (DuplicateException ex) {
+                    Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            for (int i = 0; i < numberOfPursuers; i++) {
+                try {
+                    prob.newConstraint(i + "can only chase one evader").setType(LTE).setRightHandSide(1.0);
+                } catch (DuplicateException ex) {
+                    Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } else {
+            for (int i = 0; i < numberOfPursuers; i++) {
+                try {
+
+                    prob.newConstraint(i + "is chasing").setType(EQL).setRightHandSide(1.0);
+                } catch (DuplicateException ex) {
+                    Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        
+        for (int i = 0; i < numberOfPursuers; i++) {
+            for (int j = 0; j < numberOfEvaders; j++) {
+                try {
+                    prob.setCoefficientAt(i + "can only chase one evader", "x" + i + " " + j, 1.0);
+                    prob.setCoefficientAt(j + "is chased", "x" + i + " " + j, 1.0);                    
+                } catch (NotFoundException ex) {
+                    Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                try{
+                    prob.setCoefficientAt(i + "is chasing", "x" + i + " " + j, 1.0);                    
+                } catch (NotFoundException ex) {
+                    Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+            
+        }
+
     }
 }
+
+
+    
+    
+    
