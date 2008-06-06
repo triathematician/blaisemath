70b5013de9a37a59446c1bba87dc2aa6568b4a8a
lucas.gebhart@usma.edu <lucas.gebhart@usma.edu@031c92da-8645-0410-8243-e70c107b35c0>
2008-06-06 22:28:40 +0000

    Resolved Conflicts

--- PursuitEvasion/src/tasking/ControlOptimal.java
+++ PursuitEvasion/src/tasking/ControlOptimal.java
@@ -4,14 +4,23 @@
  */
 package tasking;
 
+import drasys.or.matrix.VectorI;
 import drasys.or.mp.Constraint;
 import drasys.or.mp.ConstraintI;
+import drasys.or.mp.ConvergenceException;
 import drasys.or.mp.DuplicateException;
+import drasys.or.mp.InfeasibleException;
+import drasys.or.mp.InvalidException;
+import drasys.or.mp.NoSolutionException;
 import drasys.or.mp.NotFoundException;
 import drasys.or.mp.Problem;
+import drasys.or.mp.ScaleException;
 import drasys.or.mp.SizableProblemI;
+import drasys.or.mp.UnboundedException;
 import drasys.or.mp.Variable;
 import drasys.or.mp.VariableI;
+import drasys.or.mp.lp.DenseSimplex;
+import drasys.or.mp.lp.LinearProgrammingI;
 import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -40,73 +49,95 @@ public class ControlOptimal extends TaskGenerator {
      */
     @Override
     public void generate(Collection<Agent> team, DistanceTable table, double priority) {
-        Vector<Agent> ps = new Vector<Agent>(team);
-        Vector<Agent> es = new Vector<Agent>(target.getActiveAgents());
-        DistanceTable dist = new DistanceTable(ps, es);
-        int numberOfPursuers = ps.size();
-        int numberOfEvaders = es.size();
+        try {
+            Vector<Agent> ps = new Vector<Agent>(team);
+            Vector<Agent> es = new Vector<Agent>(target.getActiveAgents());
+            DistanceTable dist = new DistanceTable(ps, es);
+            int numberOfPursuers = ps.size();
+            int numberOfEvaders = es.size();
 
-        //  This defines (how many constaints, how many variables)      
-        SizableProblemI prob = new Problem(numberOfPursuers + numberOfEvaders,
-                numberOfPursuers * numberOfEvaders);
-        prob.getMetadata().put("lp.isMinimize", "true");
+            //  This defines (how many constaints, how many variables)
+            SizableProblemI prob = new Problem(numberOfPursuers + numberOfEvaders, numberOfPursuers * numberOfEvaders);
+            prob.getMetadata().put("lp.isMinimize", "true");
 
-        for (int i = 0; i < numberOfPursuers; i++) {
-            for (int j = 0; j < numberOfEvaders; j++) {
-                try {
-                    prob.newVariable("x" + i + " " + j).setObjectiveCoefficient(dist.get(i, j));
-                } catch (DuplicateException ex) {
-                    Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        }
-
-        final byte LTE = Constraint.LESS;
-        final byte GTE = Constraint.GREATER;
-        final byte EQL = Constraint.EQUAL;
-
-        if (numberOfPursuers >= numberOfEvaders) {
-            for (int j = 0; j < numberOfEvaders; j++) {
-                try {
-                    prob.newConstraint(j + "is chased").setType(GTE).setRightHandSide(1.0);
-                } catch (DuplicateException ex) {
-                    Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
             for (int i = 0; i < numberOfPursuers; i++) {
-                try {
-                    prob.newConstraint(i + "can only chase one evader").setType(LTE).setRightHandSide(1.0);
-                } catch (DuplicateException ex) {
-                    Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                for (int j = 0; j < numberOfEvaders; j++) {
+                    try {
+                        prob.newVariable("x" + i + " " + j).setObjectiveCoefficient(dist.get(i, j));
+                    } catch (DuplicateException ex) {
+                        Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
             }
-        } else {
+
+            final byte LTE = Constraint.LESS;
+            final byte GTE = Constraint.GREATER;
+            final byte EQL = Constraint.EQUAL;
+
+            if (numberOfPursuers >= numberOfEvaders) {
+                for (int j = 0; j < numberOfEvaders; j++) {
+                    try {
+                        prob.newConstraint(j + "is chased").setType(GTE).setRightHandSide(1.0);
+                    } catch (DuplicateException ex) {
+                        Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                for (int i = 0; i < numberOfPursuers; i++) {
+                    try {
+                        prob.newConstraint(i + "can only chase one evader").setType(LTE).setRightHandSide(1.0);
+                    } catch (DuplicateException ex) {
+                        Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            } else {
+                for (int i = 0; i < numberOfPursuers; i++) {
+                    try {
+
+                        prob.newConstraint(i + "is chasing").setType(EQL).setRightHandSide(1.0);
+                    } catch (DuplicateException ex) {
+                        Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+
             for (int i = 0; i < numberOfPursuers; i++) {
-                try {
-
-                    prob.newConstraint(i + "is chasing").setType(EQL).setRightHandSide(1.0);
-                } catch (DuplicateException ex) {
-                    Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                for (int j = 0; j < numberOfEvaders; j++) {
+                    try {
+                        prob.setCoefficientAt(i + "can only chase one evader", "x" + i + " " + j, 1.0);
+                        prob.setCoefficientAt(j + "is chased", "x" + i + " " + j, 1.0);
+                    } catch (NotFoundException ex) {
+                       // Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    try {
+                        prob.setCoefficientAt(i + "is chasing", "x" + i + " " + j, 1.0);
+                    } catch (NotFoundException ex) {
+                       // Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
             }
-        }
-        
-        for (int i = 0; i < numberOfPursuers; i++) {
-            for (int j = 0; j < numberOfEvaders; j++) {
-                try {
-                    prob.setCoefficientAt(i + "can only chase one evader", "x" + i + " " + j, 1.0);
-                    prob.setCoefficientAt(j + "is chased", "x" + i + " " + j, 1.0);                    
-                } catch (NotFoundException ex) {
-                    Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                try{
-                    prob.setCoefficientAt(i + "is chasing", "x" + i + " " + j, 1.0);                    
-                } catch (NotFoundException ex) {
-                    Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+            LinearProgrammingI lp = new DenseSimplex(prob);
+            double ans = lp.solve();
+            VectorI solution = lp.getSolution();
+            for (int i = 0; i < numberOfPursuers; i++) {
+                for (int j = 0; j < numberOfEvaders; j++) {
+                    if (solution.elementAt(j + i * numberOfEvaders)==1) {
+                        ps.get(i).assign(new Task(this, es.get(j).loc, goalType, priority));
+                    }
                 }
             }
 
-            
+            } catch  (NoSolutionException ex) {
+            Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnboundedException ex) {
+            Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InfeasibleException ex) {
+            Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ConvergenceException ex) {
+            Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ScaleException ex) {
+            Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidException ex) {
+            Logger.getLogger(ControlOptimal.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
