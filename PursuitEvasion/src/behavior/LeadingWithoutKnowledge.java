d22c9ac9aee373c14ac606b41127d0bea27a5fa7
lucas.gebhart@usma.edu <lucas.gebhart@usma.edu@031c92da-8645-0410-8243-e70c107b35c0>
2008-06-11 20:42:07 +0000

    Added more iterations of circles. Isn't circling properly.

--- PursuitEvasion/src/behavior/LeadingWithoutKnowledge.java
+++ PursuitEvasion/src/behavior/LeadingWithoutKnowledge.java
@@ -14,8 +14,14 @@ import scio.coordinate.V2;
  * Moves towards a position in front of the target which depends upon the leadFactor.
  */
 public class LeadingWithoutKnowledge extends behavior.Behavior {
-double counter = 0.0;
-   
+// starts counter that iterates in the function below allowing the agents to move in enlargening circles
+// factors refer to a factor of the sensor radius, allowing for easy change to the size of the circles
+// stepSize refers to the amount of time each step represents. With velocity, this determines how far an
+// agent moves during each size.
+    double counter = 0.0;
+    double stepSize = 0.1;
+    double startCircleFactor = 0.5;
+    double newCircleFactor =.5;
 
     /**
      * Computes desired direction of travel
@@ -29,25 +35,39 @@ double counter = 0.0;
         double y = 0;
         //System.out.println("counter: "+counter);
         if (target == null) {
-            if (counter >= 0 && counter <= (Math.PI * .5 * self.getSensorRange()) / (self.getTopSpeed() * .1)) {
-                x = .5 * self.getSensorRange() * Math.sin((counter * self.getTopSpeed() * .1) / (.5 * self.getSensorRange()));
+            counter++;
+            if (counter >= 0 && counter <= (Math.PI * startCircleFactor * self.getSensorRange()) / (self.getTopSpeed() * .1)) {
+                x = startCircleFactor * self.getSensorRange() * Math.sin((counter * self.getTopSpeed() * .1) / (startCircleFactor* self.getSensorRange()));
 
-                y = .5 * self.getSensorRange() - .5 * self.getSensorRange() * Math.cos((counter * self.getTopSpeed() * .1) / (.5 * self.getSensorRange()));
-                counter++;
+                y = startCircleFactor * self.getSensorRange() - startCircleFactor * self.getSensorRange() * Math.cos((counter * self.getTopSpeed() * .1) / (startCircleFactor * self.getSensorRange()));
+                
                 return new R2(x, y);
-            } else if (counter > (Math.PI * .5 * self.getSensorRange()) / (self.getTopSpeed() * .1) && counter <= (Math.PI * self.getSensorRange()) / (self.getTopSpeed() * .1)) {
-                x = -1 * self.getSensorRange() * Math.sin((counter * self.getTopSpeed() * .1) / (.5 * self.getSensorRange()) - Math.PI);
+            
+            } else if (counter > (Math.PI *2* startCircleFactor * self.getSensorRange()) / (self.getTopSpeed() * .1) && counter <= (Math.PI * 2*startCircleFactor*self.getSensorRange()) / (self.getTopSpeed() * .1)) {
+                x = -2*startCircleFactor * self.getSensorRange() * Math.sin((counter * self.getTopSpeed() * .1) / (startCircleFactor * self.getSensorRange()) - Math.PI);
 
-                y = self.getSensorRange() * Math.cos((counter * self.getTopSpeed() * .1) / (.5 * self.getSensorRange()) - Math.PI);
-              
+                y = 2*startCircleFactor*self.getSensorRange() * Math.cos((counter * self.getTopSpeed() * .1) / (startCircleFactor * self.getSensorRange()) - Math.PI);
+
                 return new R2(x, y);
-            } 
-            else {
+            
+            } else if (counter > (Math.PI * 2*startCircleFactor*self.getSensorRange()) / (self.getTopSpeed() * .1) && counter <= (Math.PI * 3*startCircleFactor*self.getSensorRange()))  {  
+                
+                x = -3*startCircleFactor * self.getSensorRange() * Math.sin((counter * self.getTopSpeed() * .1) / (startCircleFactor * self.getSensorRange()) - Math.PI);
+                
+                y= startCircleFactor*self.getSensorRange()+3*startCircleFactor*self.getSensorRange()*Math.cos((counter * self.getTopSpeed() * .1) / (startCircleFactor * self.getSensorRange()) - Math.PI);
+                    
+               
+            } else if ( counter> (Math.PI * 3*startCircleFactor*self.getSensorRange()) && counter <=(Math.PI * 5*startCircleFactor*self.getSensorRange())){
+                x=-4*startCircleFactor * self.getSensorRange() * Math.sin((counter * self.getTopSpeed() * .1) / (startCircleFactor * self.getSensorRange()) - 1.5*Math.PI);
+                
+                y=4*startCircleFactor*self.getSensorRange() * Math.cos((counter * self.getTopSpeed() * .1) / (startCircleFactor * self.getSensorRange()) - 1.5*Math.PI);
+                
+            } else {
+                counter =0.0;
                 return R2.ORIGIN;
             }
-            counter = 0.0;
         }
-       
+        counter = 0.0;
         if (target.v.magnitude() == 0) {
             if (self.loc.distance(target) < 15) {
                 return R2.ORIGIN;
