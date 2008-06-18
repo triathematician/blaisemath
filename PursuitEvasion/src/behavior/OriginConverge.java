e48986522b57c9098cf7435ab7741d3572c47628
lucas.gebhart@usma.edu <lucas.gebhart@usma.edu@031c92da-8645-0410-8243-e70c107b35c0>
2008-06-18 14:24:11 +0000

    Still working on this.

--- PursuitEvasion/src/behavior/OriginConverge.java
+++ PursuitEvasion/src/behavior/OriginConverge.java
@@ -12,11 +12,76 @@ import scio.coordinate.V2;
 /**
  * @author Elisha Peterson<br><br>
  * 
- * Behavior moving directly towards an agent.
+ * Behavior moving directly towards the origin to look for targets when they aren't present..
  */
-public class OriginConverge extends behavior.Behavior {   
+public class OriginConverge extends behavior.Behavior { 
+    double start = 0.0;
+    double counter = 0.0;
     public R2 direction(Agent self,V2 target,double t){
+        double x = 0.0;
+        double y = 0.0;
+        if (target == null){
+        if((self.loc) == (0,0)){
+            start++;           
+        }
+        
+        if (start =0.0) {
+             return (target.plus(target.v.multipliedBy(self.getLeadFactor() * self.loc.distance(target) / self.getTopSpeed()))).minus(self.loc).normalized();
+        }
+        else if(start>0){
+            counter++;
+            if (counter >= 0 && counter <= ((Math.PI * self.getSensorRange()) / (self.getTopSpeed() * .1))) {
+                x = (.5 * self.getSensorRange()) * Math.sin((counter * self.getTopSpeed() * .1) / (.5 * self.getSensorRange()));
+
+                y = .5 * self.getSensorRange() - (.5 * self.getSensorRange()) * Math.cos((counter * self.getTopSpeed() * .1) / (.5 * self.getSensorRange()));
+
+                return new R2(x, y);
+
+            } else if (counter > (Math.PI * self.getSensorRange()) / (self.getTopSpeed() * .1) && counter <= (3 * Math.PI * self.getSensorRange()) / (self.getTopSpeed() * .1)) {
+                x = (-2 * self.getSensorRange()) * Math.sin((counter * self.getTopSpeed() * .1) / (2 * self.getSensorRange()));
+
+                y = (2 * self.getSensorRange()) * Math.cos((counter * self.getTopSpeed() * .1) / (2 * self.getSensorRange()));
+
+                return new R2(x, y);
+
+            } else if (counter > (Math.PI * 3 * self.getSensorRange()) / (self.getTopSpeed() * .1) && counter <= (Math.PI * 6 * self.getSensorRange()) / (self.getTopSpeed() * .1)) {
+
+                x = -3 * self.getSensorRange() * Math.sin((counter * self.getTopSpeed() * .1) / (3 * self.getSensorRange()) + .5 * Math.PI);
+
+                y = 3 * self.getSensorRange() * Math.cos((counter * self.getTopSpeed() * .1) / (3 * self.getSensorRange()) + .5 * Math.PI);
+
+                return new R2(x, y);
+
+
+            } else if (counter > (Math.PI * 6 * self.getSensorRange()) / (self.getTopSpeed() * .1) && counter <= (Math.PI * 10 * self.getSensorRange()) / (self.getTopSpeed() * .1)) {
+
+                x = -4 * self.getSensorRange() * Math.sin((counter * self.getTopSpeed() * .1) / (4 * self.getSensorRange()) + Math.PI);
+
+                y = 4 * self.getSensorRange() * Math.cos((counter * self.getTopSpeed() * .1) / (4 * self.getSensorRange()) + Math.PI);
+
+                return new R2(x, y);
+
+
+            } else {
+                start = 0.0;
+                counter = 0.0;
+            }
+        }
+        start = 0.0;
+        counter = 0.0;
+        if (target.v.magnitude() == 0) {
+            if (self.loc.distance(target) <= .5 * self.getSensorRange()) {
+                return new R2(0, 0);
+            }
+            return target.minus(self.loc).normalized();
+        } else {
+            counter = 0.0;
+
+            return (target.plus(target.v.multipliedBy(self.getLeadFactor() * self.loc.distance(target) / self.getTopSpeed()))).minus(self.loc).normalized();
+        }
+        }
+           start = 0.0;     
         if(target==null){return new R2();}
         return target.minus(self.loc).normalized();
     }
-}
+
