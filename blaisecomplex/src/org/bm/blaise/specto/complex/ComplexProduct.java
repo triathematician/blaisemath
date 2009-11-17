746838de992c5de5f22181190e9da4b04a3b43b2
triathematician <triathematician@031c92da-8645-0410-8243-e70c107b35c0>
2009-11-17 18:14:05 +0000

    minor updates

--- blaisecomplex/src/org/bm/blaise/specto/complex/ComplexProduct.java
+++ blaisecomplex/src/org/bm/blaise/specto/complex/ComplexProduct.java
@@ -1,19 +1,21 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * ComplexProduct.java
+ * Created on Nov 16, 2009
  */
 
 package org.bm.blaise.specto.complex;
 
 import java.awt.geom.Point2D;
-import org.bm.blaise.specto.plottable.VPointSet;
 import org.bm.blaise.scio.coordinate.utils.ComplexMathUtils;
 
 /**
+ * <p>
+ *   Visualization of a complex product.
+ * </p>
  *
- * @author ae3263
+ * @author Elisha Peterson
  */
-public class ComplexProduct extends VPointSet<Point2D.Double> {
+public class ComplexProduct extends ComplexPointSet {
 
     static Point2D.Double[] SAMPLES = new Point2D.Double[] { new Point2D.Double(1,0), new Point2D.Double(0,1), new Point2D.Double(1,0) };
 
@@ -25,23 +27,19 @@ public class ComplexProduct extends VPointSet<Point2D.Double> {
         super(values);
     }
 
+    /**
+     *
+     * @param index the index of one of the points; only 0 and 1 are permitted
+     * @param value
+     */
     @Override
     public void setValue(int index, Point2D.Double value) {
-        values[index] = value;
-        if (index != 2) {
-            values[2] = ComplexMathUtils.product(values[0], values[1]);
+        if (index == 0 || index == 1) {
+            values[index] = value;
+            if (index != 2) {
+                values[2] = ComplexMathUtils.product(values[0], values[1]);
+            }
+            fireStateChanged();
         }
-        fireStateChanged();
-    }
-
-    /** Hook for subclasses to provide custom formatting of displayed coordinates of point. */
-    @Override
-    public String getValueString(int i) {
-        if (values[i] instanceof Point2D) {
-            Point2D p2d = ((Point2D)values[i]);
-            return formatter.format(p2d.getX()) + " + " + formatter.format(p2d.getY()) + "i";
-
-        }
-        return values[i].toString();
     }
 }
