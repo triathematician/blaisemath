5db37bcbc388e541d6cb5d9940b62e26f32bd31a
triathematician <triathematician@031c92da-8645-0410-8243-e70c107b35c0>
2010-09-20 12:18:51 +0000

    coop net analysis

---
+++ blaisegraphtheory/test/CoopNetAnalysis.java
@@ -0,0 +1,28 @@
+
+import org.bm.blaise.scio.graph.Graph;
+
+/*
+ * CoopNetAnalysis.java
+ * Created Sep 20, 2010
+ */
+
+/**
+ * Performs cooperation analyses on particular networks, in support of paper
+ * on cooperation in social networks.
+ *
+ * @author Elisha Peterson
+ */
+public class CoopNetAnalysis {
+
+    Graph network_directed;
+    Graph network_undirected;
+    Graph network_florentine;
+    Graph network_ikenet;
+
+    public void initGraphs() {
+        
+    }
+    
+
+
+}
