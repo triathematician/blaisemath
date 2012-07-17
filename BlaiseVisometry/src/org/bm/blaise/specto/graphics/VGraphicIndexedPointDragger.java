1229abe9c3eee8ed634a56acc77d096603b9b911
triathematician <triathematician@031c92da-8645-0410-8243-e70c107b35c0>
2012-07-17 20:15:02 +0000

    minor updates

--- BlaiseVisometry/src/org/bm/blaise/specto/graphics/VGraphicIndexedPointDragger.java
+++ BlaiseVisometry/src/org/bm/blaise/specto/graphics/VGraphicIndexedPointDragger.java
@@ -6,13 +6,13 @@ package org.bm.blaise.specto.graphics;
 
 import org.bm.util.DraggableIndexedPointBean;
 
-/** 
+/**
  * Implementation of an object dragger using an indexed bean pattern.
  *
  * @author elisha
  */
 public class VGraphicIndexedPointDragger<C> extends VGraphicMouseListener.Dragger<C> {
-    
+
     private DraggableIndexedPointBean<C> bean;
     private transient C beanStart;
     private transient int indexStart;
@@ -33,5 +33,13 @@ public class VGraphicIndexedPointDragger<C> extends VGraphicMouseListener.Dragge
         if (indexStart != -1)
             bean.setPoint(indexStart, beanStart, start, e.local);
     }
-    
+
+    /**
+     * Returns the index of the (most recently) active coordinate that is being dragged
+     * @return index, or -1 if there is none
+     */
+    public int getActiveIndex() {
+        return indexStart;
+    }
+
 }
