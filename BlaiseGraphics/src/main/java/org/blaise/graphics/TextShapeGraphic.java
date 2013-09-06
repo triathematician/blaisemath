defb4dcdba805aceb9300fedb4ae9de88fb44f6f
triathematician <triathematician@gmail.com>
2013-09-06 18:34:49 +0000

    BlaiseGraphics 1.0.4 - multiline string style to draw multi-line text - text shape graphic for text drawn with multiline string style Updated BlaiseVisometry & BlaiseGraphTheory to 1.0.4

---
+++ BlaiseGraphics/src/main/java/org/blaise/graphics/TextShapeGraphic.java
@@ -0,0 +1,105 @@
+/*
+ * LabeledShapeGraphic.java
+ * Created on Oct 15, 2012
+ */
+
+package org.blaise.graphics;
+
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Shape;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RectangularShape;
+import java.util.Set;
+import javax.swing.JPopupMenu;
+import org.blaise.style.BasicStringStyle;
+import org.blaise.style.MultiLineStringStyle;
+import org.blaise.style.ObjectStyler;
+import org.blaise.style.ShapeStyle;
+import org.blaise.style.VisibilityHint;
+
+/**
+ * Customizable graphic that represents a labeled item.
+ * Uses an {@link ObjectStyler} to customize appearance.
+ *
+ * @param <E> type of item
+ *
+ * @author cornidc1
+ */
+public class TextShapeGraphic<E> extends DelegatingShapeGraphic<E> {
+
+    /** Draws label */
+    private final BasicStringGraphic labelGraphic = new BasicStringGraphic(new Point2D.Double(), "");
+    
+    
+    
+    /** Special style for Text */
+    private MultiLineStringStyle labelStyle;
+    //private WrappingStringStyle labelStyle;
+
+    /**
+     * Initialize with source object.
+     * @param src source object
+     * @param rect bounding rectangle for graphic
+     * @param styler styler for source object
+     */
+    public TextShapeGraphic(E src, RectangularShape rect, ObjectStyler<E,ShapeStyle> styler) {
+        setStyler(styler);
+        setSourceObject(src);
+        setPrimitive(rect);
+    }
+
+    @Override
+    public void setSourceObject(E src) {
+        super.setSourceObject(src);
+        if (labelGraphic != null) {
+            labelGraphic.setString(src == null ? "" : getStyler() == null || getStyler().getLabelDelegate() == null ? ""+src
+                    : getStyler().getLabelDelegate().apply(src));
+            BasicStringStyle ss = (BasicStringStyle) (getStyler() == null || getStyler().getLabelStyleDelegate() == null ? null
+                                        : getStyler().getLabelStyleDelegate().apply(src));
+            labelStyle = (MultiLineStringStyle) (ss == null ? null
+                    : new MultiLineStringStyle()
+                        .anchor(ss.getAnchor())
+                        .color(ss.getColor())
+                        .font(ss.getFont())
+                        .fontSize(ss.getFontSize())
+                        .offset(ss.getOffset()));
+            labelGraphic.setStyle(labelStyle);
+        }
+    }
+
+    @Override
+    public void setPrimitive(Shape primitive) {
+        super.setPrimitive(primitive);
+    }
+
+    @Override
+    public void initialize(JPopupMenu menu, Point point, Object focus, Set<Graphic> selection) {
+        super.initialize(menu, point, getSourceObject(), selection);
+    }
+
+    @Override
+    public ShapeStyle drawStyle() {
+        final ShapeStyle parent = super.drawStyle();
+        return new ShapeStyle() {
+            public void draw(Shape primitive, Graphics2D canvas, Set<VisibilityHint> hints) {
+                boolean highlight = hints.contains(VisibilityHint.Highlight);
+                if (primitive instanceof RectangularShape) {
+                    RectangularShape rs = (RectangularShape) primitive;
+                    int inset = highlight?1:3;
+                    RectangularShape rs2 = (RectangularShape) rs.clone();
+                    rs2.setFrameFromCenter(rs.getCenterX(), rs.getCenterY(), rs.getMaxX()-inset, rs.getMaxY()-inset);
+                    primitive = rs2;
+                }
+                parent.draw(primitive, canvas, hints);
+                // must set the clip for the label style to draw properly
+                if (labelStyle != null) {
+                    labelStyle.setClip(primitive instanceof RectangularShape ? (RectangularShape) primitive : primitive.getBounds2D());
+                    labelGraphic.setPoint(new Point2D.Double(primitive.getBounds2D().getMinX()+2, primitive.getBounds2D().getMinY()+2));
+                    labelGraphic.draw(canvas);
+                }
+            }
+        };
+    }
+}
---
+++ BlaiseGraphics/src/main/java/org/blaise/style/MultiLineStringStyle.java
@@ -0,0 +1,205 @@
+/*
+ * WrappingStringStyle.java
+ * Created on Jan 2, 2013
+ */
+
+package org.blaise.style;
+
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RectangularShape;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Draws a string within clipping boundaries defined by a graphics canvas.
+ * For this to work properly, clip must be set on the canvas prior to drawing.
+ *
+ * @author cornidc1
+ */
+public class MultiLineStringStyle extends BasicStringStyle {
+
+    /** Provides clip boundaries */
+    RectangularShape clip;
+
+    public RectangularShape getClip() {
+        
+        return clip;
+    }
+
+    public void setClip(RectangularShape clip) {
+        this.clip = clip;
+    }
+    
+
+    @Override
+    public void draw(Point2D point, String string, Graphics2D canvas, Set<VisibilityHint> visibility) {
+        if (string == null || string.length() == 0) {
+            return;
+        }
+        canvas.setFont(getFont() == null ? canvas.getFont().deriveFont(getFontSize()) : getFont());
+        canvas.setColor(StyleUtils.applyHints(getColor(), visibility));
+        Shape curClip = canvas.getClip();
+        canvas.setClip(clip);
+        //clip = new Rectangle2D.Float(0,0,500,500);
+        //canvas.setClip(clip);
+
+        drawInRectangle(string, canvas, clip.getBounds2D());
+
+        //canvas.setClip(curClip);
+    }
+
+//    private void drawInEllipse(Point2D point, String string, Graphics2D canvas, Ellipse2D clip) {
+//        Font f = canvas.getFont();
+//        Rectangle2D bounds = bounds(point, string, canvas);
+//        if (clip.getWidth() * clip.getHeight() < (f.getSize() * f.getSize() / 1.5) * string.length()
+//                || clip.getWidth() < f.getSize() * 5) {
+//            canvas.setFont(f.deriveFont(f.getSize2D() - 2));
+//        }
+//
+//        setAnchor(Anchor.Center);
+//        if (bounds.getWidth() < clip.getWidth() - 8 || clip.getWidth()*.6 < 3 * f.getSize2D()) {
+//            // entire string fits in box... draw centered
+//            bounds = bounds(new Point2D.Double(clip.getCenterX(), clip.getCenterY()), string, canvas);
+//            canvas.drawString(string, (float) bounds.getX(), (float) (bounds.getY()+bounds.getHeight()));
+//        } else {
+//            // need to wrap string
+//            drawInRectangle(string, canvas,
+//                    new Rectangle2D.Double(
+//                    clip.getX()+clip.getWidth()*.15, clip.getY()+clip.getHeight()*.15,
+//                    clip.getWidth()*.7, clip.getHeight()*.7)
+//                    );
+//        }
+//    }
+//
+    private void drawInRectangle(String string, Graphics2D canvas, Rectangle2D rClip) {
+//        // make font smaller if lots of words
+//        Font f = canvas.getFont();
+//        if (rClip.getWidth() * rClip.getHeight() < (f.getSize() * f.getSize() / 1.5) * string.length()
+//                || rClip.getWidth() < f.getSize() * 5) {
+//            canvas.setFont(f.deriveFont(f.getSize2D() - 2));
+//        }
+//        float sz = canvas.getFont().getSize2D();
+        
+        Font f = canvas.getFont();
+        if (rClip.getWidth() * rClip.getHeight() < (f.getSize() * f.getSize() / 1.5) * string.length()
+                || rClip.getWidth() < f.getSize() * 5) {
+            canvas.setFont(f.deriveFont(f.getSize2D() - 2));
+        }
+        float sz = canvas.getFont().getSize2D();
+        String lns[] = string.split("\n|\r\n");
+        
+        //float boundW = 0;
+        //float boundH = 0;
+        
+        List<String> lines = new ArrayList<String>();
+        for (String s : lns){
+            //System.out.println(s);
+            //Rectangle2D rectTemp = canvas.getFontMetrics(f).getStringBounds(s,canvas);
+            //boundW = Math.max(boundW, (float)rectTemp.getWidth());
+            //boundH = boundH + (float)rectTemp.getHeight(); 
+            lines.add(s);
+        }
+        
+        
+        
+        //clip.setFrame(0, 0, boundW, boundH);
+        
+        //System.out.println("split into " + lines.size() + " lines");
+        
+        double y0;
+        switch (anchor) {
+            case North: case Northwest: case Northeast:
+                y0 = rClip.getY()+sz+2;
+                break;
+            case South: case Southwest: case Southeast:
+                y0 = rClip.getMaxY()-lines.size()*(sz+2)+sz;
+                break;
+            default:
+                y0 = rClip.getCenterY()-(lines.size()/2.0)*(sz+2)+sz;
+        }
+        for (String s : lines) {
+            double wid = canvas.getFontMetrics().getStringBounds(s, canvas).getWidth();
+            switch (anchor) {
+                case West: case Southwest: case Northwest:
+                    canvas.drawString(s, (float)(rClip.getX()+2), (float) y0);
+                    break;
+                case East: case Southeast: case Northeast:
+                    canvas.drawString(s, (float)(rClip.getMaxX()-wid-2), (float) y0);
+                    break;
+                default:
+                    canvas.drawString(s, (float)(rClip.getCenterX()-wid/2.0), (float) y0);
+            }
+            y0 += sz+2;
+        }
+    }
+//
+//    /**
+//     * Create set of lines representing the word-wrapped version of the string. Words are
+//     * wrapped at spaces if possible. Lines are constrained to be within given width and height.
+//     * If the string is too long to fit in the given space, it is truncated and "..." appended.
+//     * Assumes lines are separated by current font size + 2.
+//     * @param string initial string
+//     * @param canvas where string will be drawn (reference needed for font metrics)
+//     * @param width width of bounding box
+//     * @param height height of bounding box
+//     * @return lines
+//     */
+//    private static List<String> wrappedString(String string, Graphics2D canvas, double width, double height) {
+//        float sz = canvas.getFont().getSize2D();
+//        Rectangle2D sBounds = canvas.getFontMetrics().getStringBounds(string, canvas);
+//
+//        List<String> lines = new ArrayList<String>();
+//        if (string.length() == 0) {
+//            // do nothing
+//        } else if (width < 3*sz) {
+//            lines.add(string.substring(0,1)+"...");
+//        } else if (sBounds.getWidth() <= width - 4) {
+//            lines.add(string);
+//        } else {
+//            // need to wrap string
+//            double totHt = sz+2;
+//            int pos0 = 0;
+//            int pos1 = 1;
+//            while (pos1 < string.length()) {
+//                while (pos1 <= string.length() && canvas.getFontMetrics().getStringBounds(string.substring(pos0,pos1), canvas).getWidth() < width-4) {
+//                    pos1++;
+//                }
+//                if (pos1 < string.length()) {
+//                    int idx = string.lastIndexOf(' ', pos1 - 1);
+//                    if (idx > pos0) {
+//                        pos1 = idx + 2;
+//                    }
+//                } else {
+//                    pos1 = string.length()+1;
+//                }
+//                String s = string.substring(pos0, pos1 - 1);
+//                totHt += sz+2;
+//                if (totHt >= height-2) {
+//                    // will be the last line, may need to truncate
+//                    if (pos1-1 < string.length()) {
+//                        s = s + "...";
+//                    }
+//                    while (s.length() >= 4
+//                            && canvas.getFontMetrics().getStringBounds(s, canvas).getWidth() > width-4) {
+//                        s = s.substring(0, s.length() - 4) + "...";
+//                    }
+//                    lines.add(s);
+//                    break;
+//                } else {
+//                    lines.add(s);
+//                }
+//                pos0 = pos1 - 1;
+//                pos1 = pos0 + 1;
+//            }
+//        }
+//        return lines;
+//    }
+
+}
