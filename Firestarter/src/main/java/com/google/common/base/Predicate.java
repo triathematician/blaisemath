f9647ee25d4953eaf90d1340f67e896d58ab41f6
triathematician <triathematician@031c92da-8645-0410-8243-e70c107b35c0>
2014-10-20 12:24:38 +0000

    Firestarter - updated pom with distribution to OSS snapshot repo - changed @link to @code in Predicate.java so javadocs will build properly

--- Firestarter/src/main/java/com/google/common/base/Predicate.java
+++ Firestarter/src/main/java/com/google/common/base/Predicate.java
@@ -20,7 +20,7 @@ package com.google.common.base;
 /**
  * Determines a true or false value for a given input.
  *
- * <p>The {@link Predicates} class provides common predicates and related utilities.
+ * <p>The {@code Predicates} class provides common predicates and related utilities.
  *
  * <p>See the Guava User Guide article on <a href=
  * "http://code.google.com/p/guava-libraries/wiki/FunctionalExplained">the use of {@code
@@ -36,7 +36,7 @@ public interface Predicate<T> {
    *
    * <ul>
    * <li>Its execution does not cause any observable side effects.
-   * <li>The computation is <i>consistent with equals</i>; that is, {@link Objects#equal
+   * <li>The computation is <i>consistent with equals</i>; that is, {@code Objects#equal
    *     Objects.equal}{@code (a, b)} implies that {@code predicate.apply(a) ==
    *     predicate.apply(b))}.
    * </ul>
@@ -49,9 +49,9 @@ public interface Predicate<T> {
   /**
    * Indicates whether another object is equal to this predicate.
    *
-   * <p>Most implementations will have no reason to override the behavior of {@link Object#equals}.
+   * <p>Most implementations will have no reason to override the behavior of {@code Object#equals}.
    * However, an implementation may also choose to return {@code true} whenever {@code object} is a
-   * {@link Predicate} that it considers <i>interchangeable</i> with this one. "Interchangeable"
+   * {@code Predicate} that it considers <i>interchangeable</i> with this one. "Interchangeable"
    * <i>typically</i> means that {@code this.apply(t) == that.apply(t)} for all {@code t} of type
    * {@code T}). Note that a {@code false} result from this method does not imply that the
    * predicates are known <i>not</i> to be interchangeable.
