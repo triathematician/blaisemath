0c584c07d90c76fbfa73aaf24c783b19af5072eb
triathematician <triathematician@031c92da-8645-0410-8243-e70c107b35c0>
2015-01-13 13:06:28 +0000

    Firestarter - updated copyright dates

--- Firestarter/src/test/java/com/googlecode/blaisemath/firestarter/Testing.java
+++ Firestarter/src/test/java/com/googlecode/blaisemath/firestarter/Testing.java
@@ -4,7 +4,7 @@ package com.googlecode.blaisemath.firestarter;
  * #%L
  * Firestarter
  * --
- * Copyright (C) 2009 - 2014 Elisha Peterson
+ * Copyright (C) 2009 - 2015 Elisha Peterson
  * --
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
