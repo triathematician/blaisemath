685243bbaec94cbdc7da6b2b2fe36b0735fd11b8
triathematician <triathematician@031c92da-8645-0410-8243-e70c107b35c0>
2015-01-14 13:00:54 +0000

    BlaiseWidgets - updated pom - updated copyright dates

--- BlaiseWidgets/src/main/java/com/googlecode/blaisemath/itemvis/GraphicUpdater.java
+++ BlaiseWidgets/src/main/java/com/googlecode/blaisemath/itemvis/GraphicUpdater.java
@@ -8,7 +8,7 @@ package com.googlecode.blaisemath.itemvis;
  * #%L
  * BlaiseWidgets
  * --
- * Copyright (C) 2009 - 2014 Elisha Peterson
+ * Copyright (C) 2012 - 2015 Elisha Peterson
  * --
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
