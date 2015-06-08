cbeff69d353a85f456bb86ad88e0ac062c06d7d6
triathematician@gmail.com <triathematician@gmail.com@031c92da-8645-0410-8243-e70c107b35c0>
2015-06-08 00:48:03 +0000

    [maven-release-plugin]  copy for tag blaise-common-0.2.4

---
+++ tags/blaise-common-0.2.4/src/main/java/com/googlecode/blaisemath/util/Points.java
@@ -0,0 +1,128 @@
+package com.googlecode.blaisemath.util;
+
+/*
+ * Points.java
+ * Created Aug 28, 2013
+ */
+
+
+/*
+ * #%L
+ * BlaiseGraphTheory
+ * --
+ * Copyright (C) 2014 - 2015 Elisha Peterson
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import static com.google.common.base.Preconditions.checkArgument;
+import com.google.common.collect.Iterables;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import javax.annotation.Nullable;
+
+/**
+ * Utilities for working with points.
+ * 
+ * @author Elisha
+ */
+public class Points {
+    
+    private Points() {
+    }
+
+    /**
+     * Formats a point with n decimal places
+     * @param p the point to format
+     * @param n number of decimal places
+     * @return formatted point, e.g. (2.1,-3.0)
+     */
+    public static String formatPoint(Point2D p, int n) {
+        return String.format("(%."+n+"f, %."+n+"f)", p.getX(), p.getY());
+    }
+    
+    /**
+     * Wraps a point as a {@link DraggableCoordinate} object.
+     * @param pt the point
+     * @return wrapped instance of point as a point bean
+     */
+    public static DraggableCoordinate<Point2D> asPointBean(final Point2D pt) {
+        return new DraggableCoordinate<Point2D>() {
+            @Override
+            public Point2D getPoint() {
+                return pt;
+            }
+            @Override
+            public void setPoint(Point2D p) {
+                pt.setLocation(p);
+            }
+            @Override
+            public void setPoint(Point2D initial, Point2D dragStart, Point2D dragFinish) {
+                pt.setLocation(initial.getX()+dragFinish.getX()-dragStart.getX(),
+                        initial.getY()+dragFinish.getY()-dragStart.getY());
+            }
+        };
+    }
+   
+    /**
+     * Create and return bounding box around a given set of pounds. Returns null
+     * if there is 0 points, and a box with side length {@code margin}
+     * around the point if there is just 1 point. (If the {@code margin} is 0, returns
+     * a box of side length 1.)
+     * @param pts the points
+     * @param margin additional padding to include around the box
+     * @return bounding box
+     */
+    @Nullable 
+    public static Rectangle2D.Double boundingBox(Iterable<? extends Point2D> pts, double margin) {
+        double minx = Double.MAX_VALUE, miny = Double.MAX_VALUE, maxx = -Double.MAX_VALUE, maxy = -Double.MAX_VALUE;
+        int count = 0;
+        for (Point2D p : pts) {
+            minx = Math.min(minx, p.getX());
+            miny = Math.min(miny, p.getY());
+            maxx = Math.max(maxx, p.getX());
+            maxy = Math.max(maxy, p.getY());
+            count++;
+        }
+        if (count == 0) {
+            return null;
+        } else if (count == 1) {
+            double m = margin == 0 ? .5 : margin;
+            return new Rectangle2D.Double(minx-m, miny-m, 2*m, 2*m);
+        } else {
+            return new Rectangle2D.Double(minx-margin, miny-margin, 
+                    maxx-minx+2*margin, maxy-miny+2*margin);
+        }
+    }
+
+    /**
+     * Compute the average location of a set of points.
+     * @param locs points
+     * @return average loc
+     */
+    public static Point2D average(Iterable<? extends Point2D> locs) {
+        checkArgument(Iterables.size(locs) > 0);
+        double sumx = 0;
+        double sumy = 0;
+        int count = 0;
+        for (Point2D p : locs) {
+            sumx += p.getX();
+            sumy += p.getY();
+            count++;
+        }
+        return new Point2D.Double(sumx/count, sumy/count);
+    }
+    
+}
---
+++ tags/blaise-common-0.2.4/src/test/java/com/googlecode/blaisemath/util/UndirectedEdgeTest.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2014 Elisha.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlecode.blaisemath.util;
+
+/*
+ * #%L
+ * BlaiseGraphics
+ * --
+ * Copyright (C) 2014 - 2015 Elisha Peterson
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import com.googlecode.blaisemath.util.Edge.UndirectedEdge;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+/**
+ *
+ * @author Elisha
+ */
+public class UndirectedEdgeTest {
+    
+    @Test
+    public void testConstruct() {
+        System.out.println("construct");
+        Edge e = new Edge(1,2);
+        try {
+            UndirectedEdge e2 = new UndirectedEdge(1, null);
+            fail();
+        } catch (NullPointerException x) {
+            // expected
+        }
+        try {
+            UndirectedEdge e3 = new UndirectedEdge(null, 1);
+            fail();
+        } catch (NullPointerException x) {
+            // expected
+        }
+    }
+
+    /**
+     * Test of toString method, of class Edge.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        UndirectedEdge<Integer> inst = new UndirectedEdge<Integer>(1,2);
+        assertEquals("1:2", inst.toString());
+    }
+
+    /**
+     * Test of getNode1 method, of class Edge.
+     */
+    @Test
+    public void testGetNode1() {
+        System.out.println("getNode1");
+        UndirectedEdge<Integer> inst = new UndirectedEdge<Integer>(1,2);
+        assertEquals(1, (int) inst.getNode1());
+    }
+
+    /**
+     * Test of getNode2 method, of class Edge.
+     */
+    @Test
+    public void testGetNode2() {
+        System.out.println("getNode2");
+        UndirectedEdge<Integer> inst = new UndirectedEdge<Integer>(1,2);
+        assertEquals(2, (int) inst.getNode2());
+    }
+
+    /**
+     * Test of adjacent method, of class Edge.
+     */
+    @Test
+    public void testAdjacent() {
+        System.out.println("adjacent");
+        UndirectedEdge<Integer> inst = new UndirectedEdge<Integer>(1,2);
+        assertTrue(inst.adjacent(1));
+        assertTrue(inst.adjacent(2));
+        assertFalse(inst.adjacent(3));
+    }
+
+    /**
+     * Test of opposite method, of class Edge.
+     */
+    @Test
+    public void testOpposite() {
+        System.out.println("opposite");
+        UndirectedEdge<Integer> inst = new UndirectedEdge<Integer>(1,2);
+        assertEquals(1, (int) inst.opposite(2));
+        assertEquals(2, (int) inst.opposite(1));
+        
+        UndirectedEdge<Integer> inst2 = new UndirectedEdge<Integer>(1,1);
+        assertEquals(1, (int) inst2.opposite(1));
+    }
+
+    /**
+     * Test of equals method, of class Edge.
+     */
+    @Test
+    public void testEquals() {
+        System.out.println("equals");
+        UndirectedEdge<Integer> e1 = new UndirectedEdge<Integer>(1,2);
+        UndirectedEdge<Integer> e1b = new UndirectedEdge<Integer>(1,2);
+        UndirectedEdge<Integer> e2 = new UndirectedEdge<Integer>(2,1);
+        UndirectedEdge<Integer> e3 = new UndirectedEdge<Integer>(1,3);
+        UndirectedEdge<Integer> e4 = new UndirectedEdge<Integer>(1,1);
+        UndirectedEdge<String> e5 = new UndirectedEdge<String>("","");
+        assertTrue(e1.equals(e1b));
+        assertTrue(e1.equals(e2));
+        assertFalse(e1.equals(e3));
+        assertFalse(e1.equals(e4));
+        assertFalse(e1.equals(e5));
+        assertFalse(e1.equals(new Edge<Integer>(1,2)));
+    }
+
+    /**
+     * Test of hashCode method, of class Edge.
+     */
+    @Test
+    public void testHashCode() {
+        System.out.println("hashCode");
+        UndirectedEdge<Integer> e1 = new UndirectedEdge<Integer>(1,2);
+        UndirectedEdge<Integer> e1b = new UndirectedEdge<Integer>(1,2);
+        UndirectedEdge<Integer> e2 = new UndirectedEdge<Integer>(2,1);
+        assertTrue(e1.hashCode() == e1b.hashCode());
+        assertTrue(e1.hashCode() == e2.hashCode());
+    }
+    
+}
