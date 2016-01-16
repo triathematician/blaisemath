06c3654300bb114d93e60e42247ba319a3e3da31
Elisha Peterson <triathematician@gmail.com>
2016-01-16 07:48:32 -0500

    Firestarter update license files for 2016

--- Firestarter/src/main/java/com/googlecode/blaisemath/util/ReflectionUtils.java
+++ Firestarter/src/main/java/com/googlecode/blaisemath/util/ReflectionUtils.java
@@ -7,7 +7,7 @@ package com.googlecode.blaisemath.util;
  * #%L
  * Firestarter
  * --
- * Copyright (C) 2009 - 2015 Elisha Peterson
+ * Copyright (C) 2009 - 2016 Elisha Peterson
  * --
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
