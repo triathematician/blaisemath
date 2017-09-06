90b95ed4e7f2c0707fdbf6cb8ddf673ead13fa02
Elisha Peterson <elisha.peterson@jhuapl.edu>
2017-09-06 15:27:04 -0400

    #139 updating graphics composite

--- blaise-graphics/src/main/java/com/googlecode/blaisemath/graphics/core/UpdatingGraphicComposite.java
+++ blaise-graphics/src/main/java/com/googlecode/blaisemath/graphics/core/UpdatingGraphicComposite.java
@@ -1,5 +1,27 @@
 package com.googlecode.blaisemath.graphics.core;
 
+/*
+ * #%L
+ * blaise-graphics
+ * --
+ * Copyright (C) 2009 - 2017 Elisha Peterson
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import com.google.common.annotations.Beta;
 import com.google.common.base.Function;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
@@ -18,6 +40,7 @@ import java.util.Set;
  * @author petereb1
  * @param <T>
  */
+@Beta
 public class UpdatingGraphicComposite<T> {
 
     /** Contains the graphic elements */
