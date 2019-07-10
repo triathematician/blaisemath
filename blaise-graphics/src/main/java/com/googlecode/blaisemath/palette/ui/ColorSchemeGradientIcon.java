a12e89a54b56d13e818ff3998acfb99daa787770
Elisha Peterson <elisha.peterson@jhuapl.edu>
2019-07-10 15:52:26 -0400

    #219 initial support for palettes

--- blaise-graphics/src/main/java/com/googlecode/blaisemath/palette/ui/ColorSchemeGradientIcon.java
+++ blaise-graphics/src/main/java/com/googlecode/blaisemath/palette/ui/ColorSchemeGradientIcon.java
@@ -1,17 +1,26 @@
 package com.googlecode.blaisemath.palette.ui;
 
-/*-
+/*
  * #%L
- * ******************************* UNCLASSIFIED *******************************
- * ColorSchemeGradientIcon.java
- * edu.jhuapl.vis:conjecture-legacy
- * %%
- * Copyright (C) 2012 - 2017 Johns Hopkins University Applied Physics Laboratory
- * %%
- * (c) Johns Hopkins University Applied Physics Laboratory. All Rights Reserved.
- * JHU-APL Proprietary Information. Do not disseminate without prior approval.
+ * blaise-graphics
+ * --
+ * Copyright (C) 2009 - 2019 Elisha Peterson
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * #L%
  */
+
+
 import com.google.common.base.Preconditions;
 import com.googlecode.blaisemath.palette.ColorScheme;
 import com.googlecode.blaisemath.palette.Palette;
--- blaise-graphics/src/test/java/com/googlecode/blaisemath/palette/ColorSchemeTest.java
+++ blaise-graphics/src/test/java/com/googlecode/blaisemath/palette/ColorSchemeTest.java
@@ -1,17 +1,26 @@
 package com.googlecode.blaisemath.palette;
 
-/*-
+/*
  * #%L
- * ******************************* UNCLASSIFIED *******************************
- * PalettesTest.java
- * edu.jhuapl.vis:vis-utils
- * %%
- * Copyright (C) 2016 - 2019 Johns Hopkins University Applied Physics Laboratory
- * %%
- * (c) Johns Hopkins University Applied Physics Laboratory. All Rights Reserved.
- * JHU-APL Proprietary Information. Do not disseminate without prior approval.
+ * blaise-graphics
+ * --
+ * Copyright (C) 2009 - 2019 Elisha Peterson
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * #L%
  */
+
+
 import java.util.Arrays;
 import org.junit.Test;
 
