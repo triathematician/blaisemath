bc05da664212a855e44161dbe4adeb17de3a96ed
Elisha Peterson <triathematician@gmail.com>
2021-07-05 16:44:33 -0400

    copyrights

--- blaise-graphics/src/main/java/com/googlecode/blaisemath/graphics/swing/render/WrappedTextRenderer.java
+++ blaise-graphics/src/main/java/com/googlecode/blaisemath/graphics/swing/render/WrappedTextRenderer.java
@@ -4,7 +4,7 @@ package com.googlecode.blaisemath.graphics.swing.render;
  * #%L
  * BlaiseGraphics
  * --
- * Copyright (C) 2009 - 2020 Elisha Peterson
+ * Copyright (C) 2009 - 2021 Elisha Peterson
  * --
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
